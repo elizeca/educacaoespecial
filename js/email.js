9953d4d011a18c836816f2a1d83254fc4f81ba31
Eliezer Alves <66874099+elizeca@users.noreply.github.com>
2020-08-04 19:44:42 -0300

    Update email.js

--- js/email.js
+++ js/email.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
- if('#emai==1)
     $('#botaoemail').click(function(){
         console.log('clicou no enviar email')
         toastr["success"]("", "Email com sucesso!")
     })
-})
+}) 
